a34ea4d1b658703462f76b8f7979c0aef2f20a74
stoprightthere <pmostowsky@gmail.com>
2021-11-16 17:38:26 +0300

    Add RBF and Matern kernel values plotting

--- tests/kernels/test_hyperbolic_matern.py
+++ tests/kernels/test_hyperbolic_matern.py
@@ -6,7 +6,7 @@ from geometric_kernels.kernels.geometric_kernels import MaternIntegratedKernel
 from geometric_kernels.spaces.hyperbolic import Hyperbolic
 
 _NUM_POINTS = 50
-_NU = 0.5
+_NU = 2.5
 
 
 def plot_hyperbolic_matern():
@@ -32,5 +32,43 @@ def plot_hyperbolic_matern():
     plt.show()
 
 
+def plot_distance_vs_kernel_hyperbolic():
+    hyperboloid = Hyperbolic(dim=2)
+    lengthscale = 1.0
+
+    base = np.r_[7.14142843, -5.0, -5.0]
+    point = np.r_[14.17744688, 10.0, 10.0]
+
+    geodesic = hyperboloid.metric.geodesic(initial_point=base, end_point=point)
+
+    # Data points
+    x1 = geodesic(np.linspace(0.0, 1.0, 100))
+
+    x2 = x1[-1, None]
+
+    # Compute hyperbolic distance
+    distances = hyperboloid.distance(x1, x2)
+    # Compute heat and Matérn kernels
+    heat_kernel_vals = hyperboloid.heat_kernel(
+        distances, np.array(0.5 * lengthscale ** 2)[None], num_points=1000
+    )  # Lengthscale to heat kernel t parameter
+    heat_kernel_vals_normalized = heat_kernel_vals / heat_kernel_vals[-1]
+    matern_kernel = MaternIntegratedKernel(hyperboloid, _NU, _NUM_POINTS)
+    matern_kernel_vals = matern_kernel.K(x1, x2, lengthscale=1.0)
+
+    # Plot kernel value in function of the distance
+    plt.figure(figsize=(12, 6))
+    ax = plt.gca()
+    plt.plot(distances, heat_kernel_vals_normalized, color="gold", linewidth=3)
+    plt.plot(distances, matern_kernel_vals, color="red", linewidth=1)
+    ax.tick_params(labelsize=22)
+    ax.set_xlabel("distance", fontsize=30)
+    ax.set_ylabel("k", fontsize=30)
+    ax.legend(["SE", "Matern"], fontsize=24)
+    plt.show()
+    # plt.savefig("./plot_distance_vs_kernel.png")
+
+
 if __name__ == "__main__":
     plot_hyperbolic_matern()
+    plot_distance_vs_kernel_hyperbolic()
