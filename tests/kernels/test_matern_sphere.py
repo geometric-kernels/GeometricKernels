Corrected formula of integrated Matérn kernel for cases where the manifold is compact

tests/kernels/test_matern_sphere.py
@@ -1,4 +1,3 @@
-import geomstats.visualization as visualization
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,6 +12,7 @@ def plot_distance_vs_kernel_sphere():
     hypersphere = Sphere(dim=2)
     lengthscale = 1.
 
+    # Create a set of points along a geodesic
     base = np.r_[1., 0., 0.]
     point = np.r_[-0.9, 0.1, 0.]
     point = point / np.linalg.norm(point)
@@ -30,7 +30,7 @@ def plot_distance_vs_kernel_sphere():
     heat_kernel_vals = hypersphere.heat_kernel(distances, np.array(0.5*lengthscale**2)[None])  # Lengthscale to heat kernel t parameter
     heat_kernel_vals_normalized = heat_kernel_vals / heat_kernel_vals[-1]
     matern_kernel = MaternIntegratedKernel(hypersphere, _NU, _NUM_POINTS)
-    matern_kernel_vals = matern_kernel.K(x1, x2, lengthscale=1.)  # TODO debug
+    matern_kernel_vals = matern_kernel.K(x1, x2, lengthscale=1.)
 
     # Plot kernel value in function of the distance
     plt.figure(figsize=(12, 6))