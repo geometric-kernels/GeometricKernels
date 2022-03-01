dba779f0008738a2071c30b40d0f0116fdf4daa4
Peter Mostowsky <pmostowsky@gmail.com>
2022-03-01 16:38:34 +0300

    Parametrize test_so by backend

--- tests/spaces/test_so.py
+++ tests/spaces/test_so.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import lab as B
 import numpy as np
 import pytest
 import tensorflow as tf
@@ -25,6 +26,19 @@ def random_so_matrix(dim, num):
     return q
 
 
+def to_typed_tensor(value, backend):
+    if backend == "tensorflow":
+        return tf.convert_to_tensor(value)
+    elif backend == "torch":
+        return torch.tensor(value)
+    elif backend == "numpy":
+        return value
+    elif backend == "jax":
+        return jnp.array(value)
+    else:
+        raise ValueError("Unknown backend: {}".format(backend))
+
+
 # parametrize by dimension
 @pytest.fixture(scope="module", params=[3, 5, 6, 7, 8, 9, 10])
 def so_kernel(request):
@@ -36,77 +50,23 @@ def so_kernel(request):
     return kernel, params, state
 
 
+# parametrize by backend
 # parametrize by size
+@pytest.mark.parametrize("backend", ["numpy", "jax", "tensorflow", "torch"])
 @pytest.mark.parametrize("size", [5, 6, 7, 8, 9, 10])
-def test_so_group(so_kernel, size):
+def test_so_group(so_kernel, backend, size):
     kernel, params, state = so_kernel
 
     dim = kernel.space.dimension
-    rank = dim // 2
-    so_dim = 2 * rank ** 2 + rank if dim % 2 else 2 * rank ** 2 - rank
-    print(f"\ntesting dim={dim} (dimSO={so_dim}, rank={rank})")
 
     m = random_so_matrix(dim, size)
 
-    K = kernel.K(params, state, m, m)
+    M = to_typed_tensor(m, backend)
 
-    assert K.shape == (size, size)
-
-    np.linalg.cholesky(K + 1e-6 * np.eye(K.shape[0]))
-
-
-def test_so_kernel_torch(so_kernel):
-    kernel, params, state = so_kernel
-
-    dim = kernel.space.dimension
-
-    m = random_so_matrix(dim=dim, num=3)
-
-    M = torch.tensor(m)  # [7, 6, 6]
     K = kernel.K(params, state, M, M)
 
-    torch.linalg.cholesky(K + torch.tensor(1e-6) * torch.eye(K.shape[0]))
+    k = B.to_numpy(K)
 
+    assert k.shape == (size, size)
 
-def test_so_kernel_tf(so_kernel):
-    kernel, params, state = so_kernel
-
-    dim = kernel.space.dimension
-
-    m = random_so_matrix(dim=dim, num=3)
-
-    M = tf.convert_to_tensor(m)
-    K = kernel.K(params, state, M, M)
-
-    tf.linalg.cholesky(K + 1e-6 * tf.eye(K.shape[0], dtype=K.dtype))
-
-
-def test_so_kernel_jax(so_kernel):
-    kernel, params, state = so_kernel
-
-    dim = kernel.space.dimension
-
-    m = random_so_matrix(dim=dim, num=3)
-
-    M = jnp.array(m)
-    K = kernel.K(params, state, M, M)
-
-    jnp.linalg.cholesky(K + 1e-7 * jnp.eye(K.shape[0]))
-
-
-if __name__ == "__main__":
-    import geometric_kernels.jax  # noqa
-    import geometric_kernels.tensorflow  # noqa
-    import geometric_kernels.torch  # noqa
-
-    so_group = SpecialOrthogonalGroup(n=5)
-
-    kernel = MaternKarhunenLoeveKernel(so_group, 16)
-    params, state = kernel.init_params_and_state()
-    test_so_group((kernel, params, state), size=3)
-
-    test_so_kernel_torch((kernel, params, state))
-
-    test_so_kernel_tf((kernel, params, state))
-
-    test_so_kernel_jax((kernel, params, state))
+    np.linalg.cholesky(k + 1e-6 * np.eye(K.shape[0]))
