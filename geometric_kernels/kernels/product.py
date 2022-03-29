ff3b6dcdba4b24359a8ef3b0c0e13345cf580001
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2022-03-29 14:15:59 +0200

    bug fixes

--- geometric_kernels/kernels/product.py
+++ geometric_kernels/kernels/product.py
@@ -18,13 +18,13 @@ class ProductGeometricKernel(BaseGeometricKernel):
     def __init__(self, *kernels):
         self.kernels = kernels
 
-    dimensions = [kernel.space.dimension for kernel in self.kernels]
-    self.dimension_indices = []
-    i = 0
-    inds = B.linspace(0, sum(dimensions) - 1, sum(dimensions)).astype(int)
-    for dim in dimensions:
-        self.dimension_indices.append(inds[i : i + dim])
-        i += dim
+        dimensions = [kernel.space.dimension for kernel in self.kernels]
+        self.dimension_indices = []
+        i = 0
+        inds = B.linspace(0, sum(dimensions) - 1, sum(dimensions)).astype(int)
+        for dim in dimensions:
+            self.dimension_indices.append(inds[i : i + dim])
+            i += dim
 
     @property
     def space(self) -> List[Space]:
@@ -43,7 +43,10 @@ class ProductGeometricKernel(BaseGeometricKernel):
         X2s = [B.take(X2, inds, axis=-1) for inds in self.dimension_indices]
 
         return B.stack(
-            *[kernel.K(X, X2, p, s) for X, X2, p, s in zip(Xs, X2s, params, state)],
+            *[
+                kernel.K(X, X2, p, s)
+                for kernel, X, X2, p, s in zip(self.kernels, Xs, X2s, params, state)
+            ],
             axis=-1,
         ).prod(dim=-1)
 
@@ -51,6 +54,9 @@ class ProductGeometricKernel(BaseGeometricKernel):
         Xs = [B.take(X, inds, axis=-1) for inds in self.dimension_indices]
 
         return B.stack(
-            *[kernel.K_diag(X, p, s) for X, p, s in zip(Xs, params, state)],
+            *[
+                kernel.K_diag(X, p, s)
+                for kernel, X, p, s in zip(self.kernels, Xs, params, state)
+            ],
             axis=-1,
         ).prod(dim=-1)
