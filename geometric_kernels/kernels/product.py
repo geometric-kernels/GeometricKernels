10debd00a239b7177857a813305ad2155f088f80
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2022-04-04 12:25:54 +0200

    Allow specification of which indicies in the input corrsepond to which spaces in the product of kernels

--- geometric_kernels/kernels/product.py
+++ geometric_kernels/kernels/product.py
@@ -13,18 +13,39 @@ class ProductGeometricKernel(BaseGeometricKernel):
     """
     Basic implementation of product kernels.
     TODO: Support combined RFF/KL expansion methods
+
+    Parameters
+    ----------
+    kernels : BaseGeometricKernel
+        kernels to compute the product for
+    dimension_indices : List[B.Numeric], optional
+        List of indices the corespond to the indices of the input that should be fed to each kernel.
+        If None, assume the each kernel takes kernel.space.dimension inputs, and that the input will
+        be a stack of this size, by default None
     """
 
-    def __init__(self, *kernels):
+    def __init__(
+        self,
+        *kernels: BaseGeometricKernel,
+        dimension_indices: List[B.Numeric] = None,
+    ):
         self.kernels = kernels
 
-        dimensions = [kernel.space.dimension for kernel in self.kernels]
-        self.dimension_indices = []
-        i = 0
-        inds = B.linspace(0, sum(dimensions) - 1, sum(dimensions)).astype(int)
-        for dim in dimensions:
-            self.dimension_indices.append(inds[i : i + dim])
-            i += dim
+        if dimension_indices is None:
+            dimensions = [kernel.space.dimension for kernel in self.kernels]
+            self.dimension_indices = []
+            i = 0
+            inds = B.linspace(0, sum(dimensions) - 1, sum(dimensions)).astype(int)
+            for dim in dimensions:
+                self.dimension_indices.append(inds[i : i + dim])
+                i += dim
+        else:
+            assert len(dimension_indices) == len(self.kernels)
+            for i in dimension_indices:
+                assert i.dtype == B.Int
+                assert (i >= 0).all()
+
+            self.dimension_indices = dimension_indices
 
     @property
     def space(self) -> List[Space]:
