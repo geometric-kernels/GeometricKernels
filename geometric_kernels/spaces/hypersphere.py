4745305133115198575967e2dc3570dfeafccca3
Noemie Jaquier <noemie.jaquier@gmail.com>
2022-04-11 16:21:33 +0200

    Added Hessian on sphere

--- geometric_kernels/spaces/hypersphere.py
+++ geometric_kernels/spaces/hypersphere.py
@@ -170,3 +170,12 @@ class Hypersphere(DiscreteSpectrumSpace, gs.geometry.hypersphere.Hypersphere):
             eigenfunctions.num_eigenfunctions_per_level,
         )  # [num,]
         return B.reshape(eigenvalues, -1, 1)  # [num, 1]
+
+    def ehess2rhess(self, x, egrad, ehess, direction):
+        """
+        Riemannian Hessian along a given direction computed from the Euclidean Hessian
+
+        :return: [dim] array containing Hess_f(x)[direction]
+        """
+        normal_gradient = egrad - self.to_tangent(egrad, x)
+        return self.to_tangent(ehess, x) - self.metric.inner_product(x, normal_gradient, x) * direction
