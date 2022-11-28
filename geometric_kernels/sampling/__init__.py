91d4b54b5c76a7553363e6e32bb9cf3682d99b79
stoprightthere <pmostowsky@gmail.com>
2022-11-28 17:37:20 +0300

    Simple sampler interface

---
+++ geometric_kernels/sampling/__init__.py
@@ -0,0 +1,6 @@
+"""
+Sampler baseclass and specific implementations for kernels.
+"""
+# noqa: F401
+from geometric_kernels.sampling.base import BaseSampler
+from geometric_kernels.sampling.fourier import FourierSampler
---
+++ geometric_kernels/sampling/fourier.py
@@ -0,0 +1,27 @@
+"""
+Deterministic Fourier features sampler.
+"""
+
+import lab as B
+
+from geometric_kernels.sampling.base import BaseSampler
+
+
+class FourierSampler(BaseSampler):
+    """
+    A sampler that uses Fourier (...) features to sample from a GP.
+    """
+
+    def sample(self, feature_map, X, s=1, key=None, **kwargs):
+
+        features = feature_map(X)  # [N, M]
+
+        key = key or B.global_random_state(B.dtype(X))
+
+        num_features = B.shape(features)[-1]
+
+        key, random_weights = B.randn(key, num_features, s)  # [M, S]
+
+        random_sample = B.matmul(features, random_weights)  # [N, S]
+
+        return key, random_sample
