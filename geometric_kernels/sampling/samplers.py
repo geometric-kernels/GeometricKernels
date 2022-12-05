32a3bdee10c5ba09d5534af34a211e65c9a44505
Peter Mostowsky <pmostowsky@gmail.com>
2022-12-05 19:21:20 +0300

    Further simplify samplers and improve sampling notebook

--- geometric_kernels/sampling/samplers.py
+++ geometric_kernels/sampling/samplers.py
@@ -9,27 +9,34 @@ import lab as B
 from geometric_kernels.types import FeatureMap
 
 
+def sample_path(feature_map, s, X: B.Numeric, key=None) -> Tuple[Any, Any]:
+    """
+    Given a `feature_map`, compute `s` samples at `X` defined by random state `key`.
+    """
+    features = feature_map(X)  # [N, M]
+
+    key = key or B.global_random_state(B.dtype(X))
+
+    num_features = B.shape(features)[-1]
+
+    key, random_weights = B.randn(key, B.dtype(X), num_features, s)  # [M, S]
+
+    random_sample = B.matmul(features, random_weights)  # [N, S]
+
+    return key, random_sample
+
+
+
 def sampler(
-    feature_map: FeatureMap, s: Optional[int] = 1, key: Optional[Any] = None, **kwargs
+    feature_map: FeatureMap, s: Optional[int] = 1, **kwargs
 ) -> Callable[[Any], Any]:
     """
+    A helper wrapper around `sample_path`.
+
     Given a `feature_map`, return a function that computes `s` samples with `key` random state at given points in space.
     """
 
-    def _sample(feature_map, s, key, X: B.Numeric) -> Tuple[Any, Any]:
-        features = feature_map(X)  # [N, M]
-
-        key = key or B.global_random_state(B.dtype(X))
-
-        num_features = B.shape(features)[-1]
-
-        key, random_weights = B.randn(key, B.dtype(X), num_features, s)  # [M, S]
-
-        random_sample = B.matmul(features, random_weights)  # [N, S]
-
-        return key, random_sample
-
-    sample_f = partial(_sample, feature_map, s, key)
-    sample_f.__doc__ == "Compute random samples at `X`"
+    sample_f = partial(sample_path, feature_map, s)
+    sample_f.__doc__ == sample_path.__doc__
 
     return sample_f
