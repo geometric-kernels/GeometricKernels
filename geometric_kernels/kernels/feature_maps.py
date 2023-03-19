5ecdd677cd2f8c70cd9273c1def6f87aaacccb38
stoprightthere <pmostowsky@gmail.com>
2023-03-19 14:29:53 +0300

    Revisit feature map interface

--- geometric_kernels/kernels/feature_maps.py
+++ geometric_kernels/kernels/feature_maps.py
@@ -16,79 +16,74 @@ from geometric_kernels.spaces import DiscreteSpectrumSpace, NoncompactSymmetricS
 def deterministic_feature_map(
     space: DiscreteSpectrumSpace,
     kernel: MaternKarhunenLoeveKernel,
-    params,
-    state,
 ):
-    assert "eigenvalues_laplacian" in state
-    assert "eigenfunctions" in state
+    def _map(X: B.Numeric, params, state) -> B.Numeric:
+        assert "eigenvalues_laplacian" in state
+        assert "eigenfunctions" in state
 
-    repeated_eigenvalues = space.get_repeated_eigenvalues(kernel.num_eigenfunctions)
-    spectrum = kernel._spectrum(
-        repeated_eigenvalues**0.5,
-        nu=params["nu"],
-        lengthscale=params["lengthscale"],
-    )
+        repeated_eigenvalues = space.get_repeated_eigenvalues(kernel.num_eigenfunctions)
+        spectrum = kernel._spectrum(
+            repeated_eigenvalues**0.5,
+            nu=params["nu"],
+            lengthscale=params["lengthscale"],
+        )
 
-    weights = B.transpose(B.power(spectrum, 0.5))  # [1, M]
-    Phi = state["eigenfunctions"]
+        weights = B.transpose(B.power(spectrum, 0.5))  # [1, M]
+        Phi = state["eigenfunctions"]
 
-    def _map(X: B.Numeric) -> B.Numeric:
         eigenfunctions = Phi.__call__(X, **params)  # [N, M]
-        return B.cast(B.dtype(X), eigenfunctions) * B.cast(
+
+        _context: Dict[str, str] = {}  # no context
+        features = B.cast(B.dtype(X), eigenfunctions) * B.cast(
             B.dtype(X), weights
         )  # [N, M]
+        return features, _context
 
-    _context: Dict[str, str] = {}  # no context
-
-    return _map, _context
+    return _map
 
 
 @dispatch
 def random_phase_feature_map(
     space: DiscreteSpectrumSpace,
     kernel: MaternKarhunenLoeveKernel,
-    params,
-    state,
-    key,
     order=100,
 ):
-    key, random_phases = space.random(key, order)  # [O, D]
-    eigenvalues = state["eigenvalues_laplacian"]
+    def _map(X: B.Numeric, params, state, key) -> B.Numeric:
+        key, random_phases = space.random(key, order)  # [O, D]
+        eigenvalues = state["eigenvalues_laplacian"]
 
-    spectrum = kernel._spectrum(
-        eigenvalues**0.5,
-        nu=params["nu"],
-        lengthscale=params["lengthscale"],
-    )
+        spectrum = kernel._spectrum(
+            eigenvalues**0.5,
+            nu=params["nu"],
+            lengthscale=params["lengthscale"],
+        )
 
-    weights = B.power(spectrum, 0.5)  # [L, 1]
-    Phi = state["eigenfunctions"]
+        weights = B.power(spectrum, 0.5)  # [L, 1]
+        Phi = state["eigenfunctions"]
 
-    def _map(X: B.Numeric) -> B.Numeric:
         # X [N, D]
         random_phases_b = B.cast(B.dtype(X), from_numpy(X, random_phases))
         embedding = B.cast(
             B.dtype(X), Phi.phi_product(X, random_phases_b, **params)
         )  # [N, O, L]
         weights_t = B.cast(B.dtype(X), B.transpose(weights))
-        return B.reshape(embedding * weights_t, B.shape(X)[0], -1)  # [N, O*L]
 
-    _context: Dict[str, str] = {"key": key}
+        features = B.reshape(embedding * weights_t, B.shape(X)[0], -1)  # [N, O*L]
+        _context: Dict[str, str] = {"key": key}
+        return features, _context
 
-    return _map, _context
+    return _map
 
 
 @dispatch
-def random_phase_feature_map(
-    space: NoncompactSymmetricSpace, params, state, key, order=100
-):
-    key, random_phases = space.random_phases(key, order)  # [O, D]
+def random_phase_feature_map(space: NoncompactSymmetricSpace, order=100):
+    def _map(X: B.Numeric, params, state, key) -> B.Numeric:
+        key, random_phases = space.random_phases(key, order)  # [O, D]
 
-    key, random_lambda = spectral_density_sample(
-        key, (order,), params, space.dimension
-    )  # [O, ]
+        key, random_lambda = spectral_density_sample(
+            key, (order,), params, space.dimension
+        )  # [O, ]
 
-    def _map(X: B.Numeric) -> B.Numeric:
         # X [N, D]
         random_phases_b = B.expand_dims(
             B.cast(B.dtype(X), from_numpy(X, random_phases))
@@ -103,8 +98,7 @@ def random_phase_feature_map(
 
         c = space.inv_harish_chandra(random_lambda_b)  # [1, O]
 
-        return p * c  # [N, O]
+        _context: Dict[str, B.types.RandomState] = {"key": key}
+        return p * c, _context  # [N, O]
 
-    _context: Dict[str, B.types.RandomState] = {"key": key}
-
-    return _map, _context
+    return _map
