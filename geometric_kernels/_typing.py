9ea5916b6a21b6c06fcaddb63f25ff11e4168bad
Viacheslav Borovitskiy <viacheslav.borovitskiy@gmail.com>
2024-03-07 12:05:06 +0100

    Add the full list of types that behave differently for Python <=3.8 and Python >=3.9.

--- geometric_kernels/_typing.py
+++ geometric_kernels/_typing.py
@@ -4,17 +4,98 @@ Types used across the package.
 This private module provides a unified interface to the otherwise different
 treatment of typing hints by Python<3.9 and Python>=3.9.
 Read more at https://beartype.readthedocs.io/en/latest/api_roar/#pep-585-deprecations
+
+We follow https://peps.python.org/pep-0585/.
 """
+import collections
+import contextlib
+import re
 from sys import version_info
 
 if version_info >= (3, 9):
-    Type = type
-    List = list
     Tuple = tuple
+    List = list
     Dict = dict
-    from collections.abc import Callable, Mapping  # noqa: F401
+    Set = set
+    FrozenSet = frozenset
+    Type = type
+
+    Deque = collections.deque
+    DefaultDict = collections.defaultdict
+    OrderedDict = collections.OrderedDict
+    Counter = collections.Counter
+    ChainMap = collections.ChainMap
+
+    AbstractSet = collections.abc.Set
+
+    ContextManager = contextlib.AbstractContextManager
+    AsyncContextManager = contextlib.AbstractAsyncContextManager
+
+    Pattern = re.Pattern
+    Match = re.Match
+
+    from collections.abc import (  # noqa: F401 isort:skip
+        Awaitable,
+        Coroutine,
+        AsyncIterable,
+        AsyncIterator,
+        AsyncGenerator,
+        Iterable,
+        Iterator,
+        Generator,
+        Reversible,
+        Container,
+        Collection,
+        Callable,
+        # Set,
+        MutableSet,
+        Mapping,
+        MutableMapping,
+        Sequence,
+        MutableSequence,
+        ByteString,
+        MappingView,
+        KeysView,
+        ItemsView,
+        ValuesView,
+    )
 else:
-    from typing import Callable, Dict, List, Mapping, Tuple, Type  # noqa: F401
+    from typing import Tuple, List, Dict, Set, FrozenSet, Type  # noqa: F401 isort:skip
+    from typing import (  # noqa: F401 isort:skip
+        Deque,
+        DefaultDict,
+        OrderedDict,
+        Counter,
+        ChainMap,
+    )
+    from typing import AbstractSet  # noqa: F401 isort:skip
+    from typing import ContextManager, AsyncContextManager  # noqa: F401 isort:skip
+    from typing import Pattern, Match  # noqa: F401 isort:skip
+    from typing import (  # noqa: F401 isort:skip
+        Awaitable,
+        Coroutine,
+        AsyncIterable,
+        AsyncIterator,
+        AsyncGenerator,
+        Iterable,
+        Iterator,
+        Generator,
+        Reversible,
+        Container,
+        Collection,
+        Callable,
+        # Set,
+        MutableSet,
+        Mapping,
+        MutableMapping,
+        Sequence,
+        MutableSequence,
+        ByteString,
+        MappingView,
+        KeysView,
+        ItemsView,
+        ValuesView,
+    )
 
 # These are the types you need to import from `typing` even after PEP 585.
 from typing import Any, Generic, Optional, TypeVar, Union  # noqa: F401
