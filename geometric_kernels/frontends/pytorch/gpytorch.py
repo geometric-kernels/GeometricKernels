a90b621b90d41993dcea4d42869fb51960e100e4
Viacheslav Borovitskiy <viacheslav.borovitskiy@gmail.com>
2024-03-08 12:35:37 +0100

    Further improve the docstring of GPyTorchGeometricKernel and type hinting therein

--- geometric_kernels/frontends/pytorch/gpytorch.py
+++ geometric_kernels/frontends/pytorch/gpytorch.py
@@ -1,10 +1,14 @@
 """
 GPyTorch kernel wrapper.
+
+A tutorial on how to use this wrapper to run Gaussian process regression on
+a geometric space is available in the `frontends/GPyTorch.ipynb <https://github.com/GPflow/GeometricKernels/blob/main/notebooks/frontends/GPyTorch.ipynb>`_
+notebook.
 """
 from typing import Union
 
 import gpytorch
-import lab as B
+import numpy as np
 import torch
 
 from geometric_kernels.kernels import BaseGeometricKernel
@@ -15,28 +19,36 @@ class GPyTorchGeometricKernel(gpytorch.kernels.Kernel):
     """
     GPyTorch wrapper for :class:`BaseGeometricKernel`.
 
+    A tutorial on how to use this wrapper to run Gaussian process regression on
+    a geometric space is available in the `frontends/GPyTorch.ipynb <https://github.com/GPflow/GeometricKernels/blob/main/notebooks/frontends/GPyTorch.ipynb>`_
+    notebook.
+
     **Note**: the `base_kernel` itself does not store any of its hyperparameters
     (like `lengthscale` and `nu`), therefore you either need to pass them down to
     this wrapper explicitly or use the default values, as provided by the
     `init_params` method of the `base_kernel`.
 
-    :param base_kernel: the kernel to wrap.
-    :type base_kernel: :class:`BaseGeometricKernel`
-    :param lengthscale: initial value of the length scale. If not given or set
-        to None, uses the default value of the `base_kernel`, as provided by
-        its `init_params` method.
-    :type lengthscale: Union[B.Float, B.TorchNumeric, B.NPNumeric], optional
-    :param nu: initial value of the length scale. If not given or set
-        to None, uses the default value of the `base_kernel`, as provided by
-        its `init_params` method.
-    :type nu: Union[B.Float, B.TorchNumeric, B.NPNumeric], optional
-    :param variance: initial value of the variance (outputscale) of the kernel,
-        defaults to 1.0.
-    :type variance: Union[B.Float, B.TorchNumeric, B.NPNumeric], optional
-    :param trainable_nu: whether or not the parameter nu is to be optimized
-        over. Cannot be True if nu is equal to infinity. You cannot change
+    :param base_kernel:
+        The kernel to wrap.
+    :param lengthscale:
+        Initial value of the length scale.
+
+        If not given or set to None, uses the default value of the
+        `base_kernel`, as provided by its `init_params` method.
+    :param nu:
+        Initial value of the smoothness parameter nu.
+
+        If not given or set to None, uses the default value of the
+        `base_kernel`, as provided by its `init_params` method.
+    :param variance:
+        Initial value of the variance (outputscale) of the kernel.
+
+        Defaults to 1.0.
+    :param trainable_nu:
+        Whether or not the parameter nu is to be optimized over.
+
+        Cannot be True if nu is equal to infinity. You cannot change
         this parameter after constructing the object. Defaults to False.
-    :type trainable_nu: bool, optional
 
     :raises ValueError: if trying to set nu = infinity together with
         trainable_nu = True.
@@ -47,9 +59,9 @@ class GPyTorchGeometricKernel(gpytorch.kernels.Kernel):
     def __init__(
         self,
         base_kernel: BaseGeometricKernel,
-        lengthscale: Union[B.Float, B.TorchNumeric, B.NPNumeric] = None,
-        nu: Union[B.Float, B.TorchNumeric, B.NPNumeric] = None,
-        variance: Union[B.Float, B.TorchNumeric, B.NPNumeric] = 1.0,
+        lengthscale: Union[float, torch.Tensor, np.ndarray] = None,
+        nu: Union[float, torch.Tensor, np.ndarray] = None,
+        variance: Union[float, torch.Tensor, np.ndarray] = 1.0,
         trainable_nu: bool = False,
         **kwargs,
     ):
