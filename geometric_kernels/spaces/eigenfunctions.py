4a696d9545284009b0006b6a5c9a60f7b005120f
imbirik <zarydim@gmail.com>
2024-04-01 17:31:54 +0100

    lint

--- geometric_kernels/spaces/eigenfunctions.py
+++ geometric_kernels/spaces/eigenfunctions.py
@@ -172,7 +172,9 @@ class EigenfunctionWithAdditionTheorem(Eigenfunctions):
         )  # [N, N2, L]
 
         if is_complex(sum_phi_phi_for_level):
-            sum_phi_phi_for_level = B.cast(dtype_complex(weights), sum_phi_phi_for_level)
+            sum_phi_phi_for_level = B.cast(
+                dtype_complex(weights), sum_phi_phi_for_level
+                )
         else:
             sum_phi_phi_for_level = B.cast(B.dtype(weights), sum_phi_phi_for_level)
 
