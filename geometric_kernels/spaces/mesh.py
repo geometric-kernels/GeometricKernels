spaces.mesh, spaces.lie_groups, spaces.so, spaces.su module documentation and type hints revised. Fixed the wrong dimension is SpecialUnitary. Forbade 1-d meshes in spaces.mesh, which do not really work out of the box
Other changes:
- Renamed MatrixLieGroup -> CompactMatrixLieGroup.
- Reran SpecialUnitary.ipynb in the light of the dimension fix.
- Added missing type hints in spaces.eigenfunctions
- Minor improvements to docstrings in spaces.circle, spaces.graph.

geometric_kernels/spaces/mesh.py
@@ -20,32 +20,38 @@ from geometric_kernels.spaces.eigenfunctions import (
 
 class Mesh(DiscreteSpectrumSpace):
     """
-    The GeometricKernels space representing the node set of any user-provided mesh.
-
-    We only support the commonly used 2-dimensional meshes (discrete counterparts
-    of surfaces, 2-dimensional manifolds in a 3-dimensional ambient space) and
-    1-dimensional meshes (discrete counterparts of curves, 1-dimensional manifolds).
-
-    We use `potpourri3d <https://github.com/nmwsharp/potpourri3d>`_ to load meshes
-    and mimic the interface of `PyMesh <https://github.com/PyMesh/PyMesh>`_.
+    The GeometricKernels space representing the node set of any
+    user-provided mesh.
 
     The elements of this space are represented by node indices, integer values
-    from 0 to n-1, where n is the number of nodes in the user-provided mesh.
+    from 0 to Nv-1, where Nv is the number of nodes in the user-provided mesh.
+
+    Each individual eigenfunction constitutes a *level*.
+
+    .. note::
+        We only support the commonly used 2-dimensional meshes (discrete
+        counterparts of surfaces, 2-dimensional manifolds in a 3-dimensional
+        ambient space).
+
+    .. note::
+        We use `potpourri3d <https://github.com/nmwsharp/potpourri3d>`_ to
+        load meshes and mimic the interface of
+        `PyMesh <https://github.com/PyMesh/PyMesh>`_.
+
+    :param vertices:
+        A [Nv, 3] array of vertex coordinates, Nv is the number of vertices.
+    :param faces:
+        A [Nf, 3] array of vertex indices that represents a
+        generalized array of faces, where Nf is the number of faces.
+
+        .. Note:
+            Only 3 vertex indices per face are supported, i.e. mesh must be
+            triangulated.
     """
 
     def __init__(self, vertices: np.ndarray, faces: np.ndarray):
-        """
-        :param vertices: A [Nv, D] array of vertex coordinates, where Nv is the number of vertices,
-            D is the dimension of the embedding space (D must be either 2 or 3).
-            Note that this corresponds to a (D-1)-dimensional mesh, a discretization of some
-            assumed (D-1)-dimensional manifold.
-        :param faces: A [Nf, 3] array of vertex indices that represents a
-            generalized array of faces, where Nf is the number of faces.
-
-            .. Note:
-                Only 3 vertex indices per face are supported
-        """
         self._vertices = vertices
+        assert self._vertices.shape[1] == 3  # make sure we all is in R^3.
         self._faces = faces
         self._eigenvalues = None
         self._eigenfunctions = None
@@ -53,16 +59,27 @@ class Mesh(DiscreteSpectrumSpace):
 
     def get_eigensystem(self, num: int) -> Tuple[np.ndarray, np.ndarray]:
         """
-        Returns the first `num` eigenvalues and eigenfunctions of the Laplace-Beltrami
-        operator on the space. Makes use of Nick Sharp's robust laplacian package
-        and Scipy's sparse linear algebra.
-
+        Returns the first `num` eigenvalues and eigenvectors of the `robust
+        Laplacian <https://github.com/nmwsharp/nonmanifold-laplacian>`_.
         Caches the solution to prevent re-computing the same values.
 
-        TODO(VD): Make sure this is the optimal way to compute this!
+        .. note::
+            If the `adjacency_matrix` was a sparse SciPy array, requesting
+            **all** eigenpairs will lead to a conversion of the sparse matrix
+            to a dense one due to scipy.sparse.linalg.eigsh limitations.
 
-        :param num: number of eigenvalues and functions to return.
-        :return: A Tuple of eigenvectors [Nv, num], eigenvalues [num, 1]
+        .. warning::
+            Always uses SciPy (thus CPU) for internal computations. We will
+            need to fix this in the future.
+
+        .. todo::
+            See warning above.
+
+        :param num:
+            Number of eigenpairs to return. Performs the computation at the
+            first call. Afterwards, fetches the result from cache.
+        :return:
+            A tuple of eigenvectors [nv, num], eigenvalues [num, 1].
         """
         if num not in self.cache:
             L, M = robust_laplacian.mesh_laplacian(self.vertices, self.faces)
@@ -81,57 +98,68 @@ class Mesh(DiscreteSpectrumSpace):
 
     def get_eigenvectors(self, num: int) -> B.Numeric:
         """
-        :param num: number of eigenvectors returned
-        :return: eigenvectors [Nv, num]
+        :param num:
+            number of eigenvectors to return.
+        :return:
+            array of eigenvectors, with shape [Nv, num].
         """
         return self.get_eigensystem(num)[0]
 
     def get_eigenvalues(self, num: int) -> B.Numeric:
         """
-        :param num: number of eigenvalues returned
-        :return: eigenvalues [num, 1]
+        :param num:
+            Number of eigenvalues to return.
+        :return:
+            Array of eigenvalues, with shape [num, 1].
         """
         return self.get_eigensystem(num)[1]
 
     def get_repeated_eigenvalues(self, num: int) -> B.Numeric:
         """
-        :param num: number of eigenvalues returned
-        :return: eigenvalues [num, 1]
+        Same as :meth:`get_eigenvalues`.
+
+        :param num:
+            Same as :meth:`get_eigenvalues`.
         """
         return self.get_eigenvalues(num)
 
     def get_eigenfunctions(self, num: int) -> Eigenfunctions:
         """
-        First `num` eigenfunctions of the Laplace-Beltrami operator on the Mesh.
+        Returns the :class:`~.EigenfunctionsFromEigenvectors` object with
+        `num` levels (i.e., in this case, `num` eigenpairs).
 
-        :param num: number of eigenfunctions returned
-        :return: eigenfunctions [Nv, num]
+        :param num:
+            Number of levels.
         """
         eigenfunctions = EigenfunctionsFromEigenvectors(self.get_eigenvectors(num))
         return eigenfunctions
 
     @property
     def num_vertices(self) -> int:
-        """Number of vertices, Nv"""
+        """
+        Number of vertices in the mesh, Nv.
+        """
         return len(self._vertices)
 
     @property
     def num_faces(self) -> int:
-        """Number of faces, Nf"""
+        """
+        Number of faces in the mesh, Nf.
+        """
         return len(self._faces)
 
     @property
     def dimension(self) -> int:
         """
-        Dimension of the space. Equal to D-1, where D is the dimension of the embedding space.
+        :return:
+            2.
         """
-        return self._vertices.shape[1] - 1
+        return 2
 
     @property
     def vertices(self) -> np.ndarray:
         """
-        A [Nv, D] array of vertex coordinates, where Nv is the number of vertices,
-        D is the dimension of the embedding space (D must be either 2 or 3).
+        A [Nv, 3] array of vertex coordinates, Nv is the number of vertices.
         """
         return self._vertices
 
@@ -146,9 +174,14 @@ class Mesh(DiscreteSpectrumSpace):
     @classmethod
     def load_mesh(cls, filename: str) -> "Mesh":
         """
-        :param filename: path to read the file from. Supported formats: `obj`,
-            `ply`, `off`, and `stl`. Format inferred automatically from the path
-            extension.
+        Construct :class:`Mesh` by loading a mesh from the file at `filename`.
+
+        :param filename:
+            Path to read the file from. Supported formats: `obj`,
+            `ply`, `off`, and `stl`. Format inferred automatically from the
+            file extension.
+        :return:
+            And object of class :class:`Mesh` representing the loaded mesh.
         """
         # load vertices and faces using potpourri3d
         vertices, faces = pp3d.read_mesh(filename)

geometric_kernels/spaces/so.py
@@ -1,6 +1,7 @@
 """
-This module provides the :class:`SpecialOrthogonal` space and the representation
-of its spectrum, the :class:`SOEigenfunctions` class.
+This module provides the :class:`SpecialOrthogonal` space, the respective
+:class:`~.eigenfunctions.Eigenfunctions` subclass :class:`SOEigenfunctions`,
+and a class :class:`SOCharacter` for representing characters of the group.
 """
 
 import itertools
@@ -11,12 +12,14 @@ from functools import reduce
 
 import lab as B
 import numpy as np
+from beartype.typing import List, Tuple
 from opt_einsum import contract as einsum
 
 from geometric_kernels.lab_extras import dtype_double, from_numpy, qr, take_along_axis
+from geometric_kernels.spaces.eigenfunctions import Eigenfunctions
 from geometric_kernels.spaces.lie_groups import (
+    CompactMatrixLieGroup,
     LieGroupCharacter,
-    MatrixLieGroup,
     WeylAdditionTheorem,
 )
 from geometric_kernels.utils.utils import (
@@ -39,7 +42,7 @@ class SOEigenfunctions(WeylAdditionTheorem):
 
         super().__init__(n, num_levels, compute_characters)
 
-    def _generate_signatures(self, num_levels):
+    def _generate_signatures(self, num_levels: int) -> List[Tuple[int]]:
         signatures = []
         # largest LB eigenvalues correspond to partitions of smallest integers
         # IF p >> k the number of partitions of p into k parts is O(p^k)
@@ -66,7 +69,7 @@ class SOEigenfunctions(WeylAdditionTheorem):
         signatures = [signatures[i] for i in min_ind]
         return signatures
 
-    def _compute_dimension(self, signature):
+    def _compute_dimension(self, signature: Tuple[int]) -> int:
         if self.n % 2 == 1:
             qs = [pk + self.rank - k - 1 / 2 for k, pk in enumerate(signature)]
             rep_dim = reduce(
@@ -102,19 +105,16 @@ class SOEigenfunctions(WeylAdditionTheorem):
             )
             return int(round(rep_dim))
 
-    def _compute_eigenvalue(self, signature):
+    def _compute_eigenvalue(self, signature: Tuple[int]) -> B.Float:
         np_sgn = np.array(signature)
         rho = self.rho
         eigenvalue = np.linalg.norm(rho + np_sgn) ** 2 - np.linalg.norm(rho) ** 2
         return eigenvalue
 
-    def _compute_character(self, n, signature):
+    def _compute_character(self, n: int, signature: Tuple[int]) -> LieGroupCharacter:
         return SOCharacter(n, signature)
 
-    def _torus_representative(self, X):
-        r"""
-        The function maps Lie Group Element X to T - a maximal torus of the Lie group
-        """
+    def _torus_representative(self, X: B.Numeric) -> B.Numeric:
         gamma = None
 
         if self.n == 3:
@@ -156,11 +156,27 @@ class SOEigenfunctions(WeylAdditionTheorem):
         return gamma
 
     def inverse(self, X: B.Numeric) -> B.Numeric:
-        return B.transpose(X)
+        return SpecialOrthogonal.inverse(X)
 
 
 class SOCharacter(LieGroupCharacter):
-    def __init__(self, n, signature):
+    """
+    The class that represents a character of the SO(n) group.
+
+    These characters are always real-valued.
+
+    These are polynomials whose coefficients are precomputed and stored in a
+    file. By default, there are 20 precomputed characters for n from 3 to 8.
+    If you want more, use the `utils/compute_characters.py` script.
+
+    :param n:
+        The order n of the SO(n) group.
+    :param signature:
+        The signature that determines a particular character (and an
+        irreducible unitary representation along with it).
+    """
+
+    def __init__(self, n: int, signature: Tuple[int]):
         self.signature = signature
         self.n = n
         self.coeffs, self.monoms = self._load()
@@ -183,30 +199,35 @@ class SOCharacter(LieGroupCharacter):
                         )
                     ) from None
 
-    def __call__(self, gammas):
+    def __call__(self, gammas: B.Numeric) -> B.Numeric:
         char_val = B.zeros(B.dtype(gammas), *gammas.shape[:-1])
         for coeff, monom in zip(self.coeffs, self.monoms):
             char_val += coeff * B.prod(gammas ** from_numpy(gammas, monom), axis=-1)
         return char_val
 
 
-class SpecialOrthogonal(MatrixLieGroup):
+class SpecialOrthogonal(CompactMatrixLieGroup):
     r"""
-    The GeometricKernels space representing the special orthogonal group
-    :math:`SO(n)` consisting of n by n orthogonal matrices with unit
-    determinant.
+    The GeometricKernels space representing the special orthogonal group SO(n)
+    consisting of n by n orthogonal matrices with unit determinant.
 
-    The elements of this space are represented as :math:`n \times n` orthogonal
+    The elements of this space are represented as n x n orthogonal
     matrices with real entries and unit determinant.
 
-    Note: we only support n >= 3. Mathematically, SO(2) is equivalent to the
-    unit circle, which is available as the :class:`Circle` space.
-    For large values of n, you might need to run the `compute_characters.py`
-    script to precompute the necessary mathematical quantities beyond the ones
-    provided by default.
+    :param n:
+        The order n of the group SO(n).
+
+    .. note::
+        We only support n >= 3. Mathematically, SO(2) is equivalent to the
+        unit circle, which is available as the :class:`~.spaces.Circle` space.
+
+        For larger values of n, you might need to run the
+        `utils/compute_characters.py` script to precompute the necessary
+        mathematical quantities beyond the ones provided by default. Same
+        can be required for larger numbers of levels.
     """
 
-    def __init__(self, n):
+    def __init__(self, n: int):
         if n < 3:
             raise ValueError("Only n >= 3 is supported. For n = 2, use Circle.")
         self.n = n
@@ -216,23 +237,29 @@ class SpecialOrthogonal(MatrixLieGroup):
 
     @property
     def dimension(self) -> int:
+        """
+        The dimension of the space, as that of a Riemannian manifold.
+
+        :return:
+            floor(n(n-1)/2) where n is the order of the group SO(n).
+        """
         return self.dim
 
-    def inverse(self, X: B.Numeric) -> B.Numeric:
-        return B.transpose(X)
+    @staticmethod
+    def inverse(X: B.Numeric) -> B.Numeric:
+        return B.transpose(X)  # B.transpose only inverses the last two dims.
 
-    def get_eigenfunctions(self, num: int) -> SOEigenfunctions:
+    def get_eigenfunctions(self, num: int) -> Eigenfunctions:
         """
-        :param num: number of eigenfunctions returned.
+        Returns the :class:`~.SOEigenfunctions` object with `num` levels
+        and order n.
+
+        :param num:
+            Number of levels.
         """
         return SOEigenfunctions(self.n, num)
 
     def get_eigenvalues(self, num: int) -> B.Numeric:
-        """
-        Eigenvalues of first 'num' levels of the Laplace-Beltrami operator.
-
-        :return: [num, 1] array containing the eigenvalues
-        """
         eigenfunctions = SOEigenfunctions(self.n, num)
         eigenvalues = np.array(
             [eigenvalue for eigenvalue in eigenfunctions._eigenvalues]
@@ -240,20 +267,15 @@ class SpecialOrthogonal(MatrixLieGroup):
         return B.reshape(eigenvalues, -1, 1)  # [num, 1]
 
     def get_repeated_eigenvalues(self, num: int) -> B.Numeric:
-        """Eigenvalues of first 'num' levels of the Laplace-Beltrami operator,
-        repeated according to their multiplicity.
-
-        :return: [M, 1] array containing the eigenvalues
-        """
         eigenfunctions = SOEigenfunctions(self.n, num)
         eigenvalues = chain(
             eigenfunctions._eigenvalues,
             [rep_dim**2 for rep_dim in eigenfunctions._dimensions],
         )
-        return B.reshape(eigenvalues, -1, 1)  # [M, 1]
+        return B.reshape(eigenvalues, -1, 1)  # [J, 1]
 
-    def random(self, key, number):
-        if self.n == 2:
+    def random(self, key: B.RandomState, number: int):
+        if self.n == 2:  # for the bright future where we support SO(2).
             # SO(2) = S^1
             key, thetas = B.random.randn(key, dtype_double(key), number, 1)
             thetas = 2 * math.pi * thetas