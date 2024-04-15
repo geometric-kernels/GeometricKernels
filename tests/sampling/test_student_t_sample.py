691285093290146348f2aed61c1e58f99eaff30d
imbirik <zarydim@gmail.com>
2024-04-16 00:35:55 +0100

    update student-t test

--- tests/sampling/test_student_t_sample.py
+++ tests/sampling/test_student_t_sample.py
@@ -1,20 +1,23 @@
 import numpy as np
 import pytest
-import scipy.stats
+from scipy.stats import multivariate_t
 
 from geometric_kernels.feature_maps.probability_densities import student_t_sample
 
 
-@pytest.mark.parametrize("deg_freedom", [2, 5, 42])
+@pytest.mark.parametrize("deg_freedom, n", [(2, 5, 42), (3, 5, 6)])
 def test_student_t_sample(deg_freedom):
     size = (1024,)
 
     key = np.random.RandomState(seed=1234)
     default_rng = np.random.default_rng(seed=1234)
 
-    _, random_sample = student_t_sample(key, size, np.r_[deg_freedom])
+    shape = 1.0*np.eye(n)
+    loc = 1.0*np.zeros((n,))
 
-    np_random_sample = default_rng.standard_t(deg_freedom, size=size)
+    _, random_sample = student_t_sample(key, loc, shape, deg_freedom, size)
+
+    np_random_sample = default_rng.multivariate_t(loc, shape, deg_freedom, size=size)
 
     p_value = 0.05
 
