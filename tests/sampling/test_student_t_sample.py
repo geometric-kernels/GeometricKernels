9e0ca3cf5b1689cf662807aaeb6129214fb3675a
imbirik <zarydim@gmail.com>
2024-04-16 00:56:18 +0100

    fix

--- tests/sampling/test_student_t_sample.py
+++ tests/sampling/test_student_t_sample.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from scipy.stats import multivariate_t
+from scipy.stats import multivariate_t, ks_2samp
 
 from geometric_kernels.feature_maps.probability_densities import student_t_sample
 
@@ -10,16 +10,15 @@ def test_student_t_sample(deg_freedom):
     size = (1024,)
 
     key = np.random.RandomState(seed=1234)
-    default_rng = np.random.default_rng(seed=1234)
 
     shape = 1.0*np.eye(n)
     loc = 1.0*np.zeros((n,))
 
     _, random_sample = student_t_sample(key, loc, shape, deg_freedom, size)
 
-    np_random_sample = default_rng.multivariate_t(loc, shape, deg_freedom, size=size)
+    np_random_sample = multivariate_t(loc, shape, deg_freedom, size=size, seed=key)
 
     p_value = 0.05
 
-    test_res = scipy.stats.ks_2samp(random_sample, np_random_sample)
+    test_res = ks_2samp(random_sample, np_random_sample)
     assert test_res.pvalue > p_value
